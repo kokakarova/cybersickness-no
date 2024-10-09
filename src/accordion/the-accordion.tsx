3aa5140044ca51fc977557823358ea2034ff9860
kokakarova <kostadinkakarova@gmail.com>
2024-10-09 16:34:21 +0200

    instal mui for accordion

---
+++ src/accordion/the-accordion.tsx
@@ -0,0 +1,46 @@
+import Accordion from "@mui/material/Accordion";
+
+import AccordionSummary from "@mui/material/AccordionSummary";
+import AccordionDetails from "@mui/material/AccordionDetails";
+import ArrowDropDownIcon from "@mui/icons-material/ArrowDropDown";
+import SingleAccordionElement from "./single-accordion-element";
+
+
+const TheAccordion = () => {
+  return (
+    <div className="accordion-container">
+      <SingleAccordionElement header="PLAYTIME" subHeader="Limit Duration + Gradual Exposure + Frequent Breaks" content="Play in short intervals and take breaks when you feel unwell. Get
+            fresh air and follow the 20-20-20 rule: every 20 minutes, look at
+            something 20 feet away for 20 seconds."/>
+    
+      <Accordion>
+        <AccordionSummary
+          expandIcon={<ArrowDropDownIcon />}
+          aria-controls="panel2-content"
+          id="panel2-header"
+        >
+          Accordion 2
+        </AccordionSummary>
+        <AccordionDetails>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+          malesuada lacus ex, sit amet blandit leo lobortis eget.
+        </AccordionDetails>
+      </Accordion>
+      <Accordion>
+        <AccordionSummary
+          //   expandIcon={<ExpandMoreIcon />}
+          aria-controls="panel3-content"
+          id="panel3-header"
+        >
+          Accordion Actions
+        </AccordionSummary>
+        <AccordionDetails>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+          malesuada lacus ex, sit amet blandit leo lobortis eget.
+        </AccordionDetails>
+      </Accordion>
+    </div>
+  );
+}
+
+export default TheAccordion;
