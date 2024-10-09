399b8dcdbea75f2458f71258169d5298a26e8b89
kokakarova <kostadinkakarova@gmail.com>
2024-10-09 16:48:17 +0200

    put data content in constants file/variable

---
+++ src/constants.ts
@@ -0,0 +1,73 @@
+import { AccordionType } from "./types";
+
+export const accordionContent: AccordionType[] = [
+  {
+    header: "PLAYTIME",
+    content: [
+      {
+        subHeader: "Limit Duration + Gradual Exposure + Frequent Breaks",
+        text: "Play in short intervals and take breaks when you feel unwell. Get fresh air and follow the 20-20-20 rule: every 20 minutes, look at something 20 feet away for 20 seconds.",
+      },
+    ],
+  },
+  {
+    header: "ENVIRONMENT",
+    content: [
+        {
+          subHeader: "LIGHTS",
+          text: "Play in a well lit room. Soft, indirect light is ideal, avoid glare and reduce strain on the eyes.",
+        },
+        {
+            subHeader: "AIR",
+            text: "Having a fan blow some air into you could trick the body into thinking that you are in fact moving."
+        }
+      ],
+  },
+  {
+    header: "EQUIPMENT",
+    content: [
+        {
+          subHeader: "MONITOR",
+          text: "Not too big, not too small. Have static objects that are outside of the monitor in your vision's scope. If you have a big monitor, sit a bit further away. Adjust brightness, contrast, and refresh rates for optimal visual comfort.",
+        },
+        {
+            subHeader: "GAMEPLAY SETTINGS",
+            text: "If possible, reduce motion blur, field of view (FOV), and camera speed to decrease discomfort."
+        },
+        {
+            subHeader: "ACUPRESSURE WRISTBANDS",
+            text: "Might look silly, but try it. Acupressure wristbands ease nausea by pressing the P6 point on the inner wrist, stimulating nerves that balance energy (Qi) and regulate nausea-related pathways."
+        },
+        {
+            subHeader: "MOTION SICKNESS GLASSES",
+            text: "This is probably even sillier, but give it a go."
+        }
+      ],
+  },
+  {
+    header: "CONSUMABLES",
+    content: [
+        {
+          subHeader: "WATER",
+          text: "Drink fresh, cool water. it help settle your stomach when you start feeling sweaty or nauseous.",
+        },
+        {
+            subHeader: "GINGER",
+            text: "Have some kind of a ginger drink or snack. It could help with the nausea."
+        }
+      ],
+  },
+  {
+    header: "OTHER TACTICS",
+    content: [
+        {
+          subHeader: "EYE EXERCISES",
+          text: "Practice focusing exercises to reduce eye strain. For example, alternate focusing on a near object and a distant one.",
+        },
+        {
+            subHeader: "MINDFULNESS AND BREATHING",
+            text: "Engage in deep-breathing exercises or mindfulness techniques to manage anxiety and discomfort."
+        }
+      ],
+  },
+];
