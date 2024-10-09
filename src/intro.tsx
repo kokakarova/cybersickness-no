855eefe2eddc41661552eb7ea3c4443d3b4f6df6
kokakarova <kostadinkakarova@gmail.com>
2024-10-09 17:04:46 +0200

    add intro text

---
+++ src/intro.tsx
@@ -0,0 +1,12 @@
+
+const Intro = () => {
+  return (
+    <div className="intro-container">
+      If you're one of the unlucky ones to experience motion sickness when
+      playing video games on you PC I offer my condolences AND this list of
+      things you could try
+    </div>
+  );
+};
+
+export default Intro;
