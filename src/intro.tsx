7f1cbb452619a08d4ed85ff98ba63edb7235d7ce
kokakarova <kostadinkakarova@gmail.com>
2024-10-09 23:26:56 +0200

    fix typo

--- src/intro.tsx
+++ src/intro.tsx
@@ -3,7 +3,7 @@ const Intro = () => {
   return (
     <div className="intro-container">
       If you're one of the unlucky ones to experience motion sickness when
-      playing video games on you PC I offer my condolences AND this list of
+      playing video games on your PC I offer my condolences AND this list of
       things you could try
     </div>
   );
